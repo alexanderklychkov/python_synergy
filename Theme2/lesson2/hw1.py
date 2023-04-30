82b5f8158403d429969ee9157832df665fb792be
Alexander Klychkov <caany408@gmail.com>
2023-04-30 14:18:51 +0300

    HW 2.2

---
+++ Theme2/lesson2/hw1.py
@@ -0,0 +1,4 @@
+long, width = map(float, input('Введите длину и ширину через пробел: ').split())
+
+print('Площадь прямоугольника -', long * width)
+print('Периметр прямоугольника -', (long + width) * 2)
