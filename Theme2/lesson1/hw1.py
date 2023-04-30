7321d84dfcee2501cf4dfcd5a1fedc6c9da24bc9
Alexander Klychkov <caany408@gmail.com>
2023-04-30 11:56:30 +0300

    HW 2.1

---
+++ Theme2/lesson1/hw1.py
@@ -0,0 +1,5 @@
+pet_species = input('Введите вид питомца: ')
+age = input('Его возраст: ')
+name = input('Его кличка: ')
+
+print(f'Это {pet_species} по кличке "{name}". Возраст: {age} года.')
---
+++ Theme2/lesson1/hw2.py
@@ -0,0 +1,14 @@
+print('Тест по этапам развития человека.')
+print()
+
+a1 = input('1 стадия - ')
+a2 = input('2 стадия - ')
+a3 = input('3 стадия - ')
+a4 = input('4 стадия - ')
+a5 = input('5 стадия - ')
+
+print('Ваш ответ:')
+print(a1, a2, a3, a4, a5, sep='=>')
+print('')
+print('Верный ответ:')
+print('Homo habilis', 'Homo erectus', 'Homo heidelbergensis', 'Homo', 'Homo sapiens', sep='=>')
